d690785b7076467b361688b4e027e0eef6b4a8cc
Wesley Senciani <wsa.wes05@gmail.com>
2024-11-30 20:19:53 -0300

    inproving view aplication

--- main.py
+++ main.py
@@ -10,24 +10,22 @@ def pegar_cep():
         texto = f"CEP: {resp['cep']}\nLogradouro: {resp['logradouro']}\nBairro: {resp['bairro']}\nCidade: {resp['localidade']}\nEstado: {resp['estado']}"
         textoCep["text"] = texto
     else:
-        textoCep["test"] = "CEP não encontrado!"
+        textoCep["text"] = "CEP não encontrado!"
 
 janela = Tk()
 janela.title("Busca CEP - Python")
-janela.geometry("400x400")
+janela.geometry("500x500")
 
-textoInicial = Label(janela, text="Coloque o cep e clique em buscar!")
+textoInicial = Label(janela, text="Coloque o CEP e clique em buscar!", font=("Arial", 16, "bold"))
 textoInicial.grid(column=0, row=0, padx=10, pady=10)
 
-cep = Entry(width=15, bg="white", font=("Comic Sans MS", 14))
+cep = Entry(width=20, bg="white", font=("Arial", 14))
 cep.grid(column=0, row=1, padx=10, pady=10)
 
-botao = Button(janela, text="Buscar", command=pegar_cep)
+botao = Button(janela, text="Buscar", bg="#3b0b66", fg="white", width=8, height=2, command=pegar_cep)
 botao.grid(column=1, row=1, padx=10, pady=10)
 
-textoCep = Label(janela, text="")
+textoCep = Label(janela, text="", font=("Arial", 14))
 textoCep.grid(column=0, row=2, padx=10, pady=10)
-
-
 
 janela.mainloop()
