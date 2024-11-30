8e8fac81a1a49f8dfde85c38d5634d7de44db4fb
Wesley Senciani <wsa.wes05@gmail.com>
2024-11-30 20:04:29 -0300

    api request done

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import requests
 from tkinter import *
 
-def mostrar():
-    texto = "Texto mostrado após apertar no botão!"
-    textoCotacoes["text"] = texto
+def pegar_cep():
+    cep_valor = cep.get()
+    requisicao = requests.get(f"https://viacep.com.br/ws/{cep_valor}/json/")
+
+    if requisicao.status_code == 200:
+        resp = requisicao.json()
+        texto = f"CEP: {resp['cep']}\nLogradouro: {resp['logradouro']}\nBairro: {resp['bairro']}\nCidade: {resp['localidade']}\nEstado: {resp['estado']}"
+        textoCep["text"] = texto
+    else:
+        textoCep["test"] = "CEP não encontrado!"
 
 janela = Tk()
-janela.title("Estudando Python")
+janela.title("Busca CEP - Python")
 janela.geometry("400x400")
 
-textoInicial = Label(janela, text="Clique no botão agora para ver o texto!")
+textoInicial = Label(janela, text="Coloque o cep e clique em buscar!")
 textoInicial.grid(column=0, row=0, padx=10, pady=10)
 
-botao = Button(janela, text="Mostrar", command=mostrar)
-botao.grid(column=0, row=1, padx=10, pady=10)
+cep = Entry(width=15, bg="white", font=("Comic Sans MS", 14))
+cep.grid(column=0, row=1, padx=10, pady=10)
 
-textoCotacoes= Label(janela, text="")
-textoCotacoes.grid(column=0, row=2, padx=10, pady=10)
+botao = Button(janela, text="Buscar", command=pegar_cep)
+botao.grid(column=1, row=1, padx=10, pady=10)
+
+textoCep = Label(janela, text="")
+textoCep.grid(column=0, row=2, padx=10, pady=10)
 
 
 
